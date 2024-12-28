155baccff8df277fb6a5ea08e373c65aa7e80f04
briteming <briteming@gmail.com>
2024-12-27 20:22:30 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var _config = {
     blog_name       : '用于演示的博客',         // 博客名称
-    owner           : 'laizimo',           // github 用户名
-    repo            : 'zimo-article',// github 中对应项目名
-    per_page        : '10',                    // 默认一页显示几篇文章
+    owner           : 'briteming',           // github 用户名
+    repo            : 'vgib',// github 中对应项目名
+    per_page        : '5',                    // 默认一页显示几篇文章
     // 版权起始年份
     since_year: 2015,
     // icp备案号 ICP_license: 京ICP备1234556号-1
